bf2b23c31bdf65694796e1a8d89ad39fca9bf751
Allie Schultz <allie.m.schultz1@gmail.com>
2025-04-28 17:42:40 -0700

    Add project files.

---
+++ WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿00// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+document.addEventListener('DOMContentLoaded', function () {
+    const videoContainer = document.getElementById('videoContainer');
+    videoContainer.style.top = '-500px';
+
+    function dropVideo() {
+        videoContainer.style.transition = 'top 3s ease-out';
+        videoContainer.style.top = '210px';
+    }
+    dropVideo();
+
+    setTimeout(dropVideo, 4000);
+});
+
+
